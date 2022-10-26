d61abc6cf5075f70ff8cd24f481836d60d3156b0
sergioalcazar745 <sergioalcazar745@gmail.com>
2022-10-26 14:25:46 +0200

    click button

--- src/components/UpdateDepartamento.js
+++ src/components/UpdateDepartamento.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Global from '../Global';
 import axios from 'axios';
 import { Navigate } from 'react-router-dom';
+import { event } from 'jquery';
 
 export default class UpdateDepartamento extends Component {
 
@@ -26,17 +27,21 @@ export default class UpdateDepartamento extends Component {
         }
 
         axios.put(request, departamento).then(response => {
-            this.setState({modificar: true})
+            this.setState({ modificar: true })
         }).catch(error => {
             console.log("Error: " + error)
         })
     }
 
+    prueba = (e) => {
+        console.log(e.target.value)
+    }
+
     cargarDepartamento = () => {
-        var request = Global.urlDepartamentos + "api/Departamentos/" + this.props.id;       
+        var request = Global.urlDepartamentos + "api/Departamentos/" + this.props.id;
 
         axios.get(request).then(response => {
-            this.setState({departamento: response.data, status: true})
+            this.setState({ departamento: response.data, status: true })
         }).catch(error => {
             console.log("Error: " + error)
         })
@@ -48,22 +53,26 @@ export default class UpdateDepartamento extends Component {
 
     render() {
 
-        if(this.state.modificar){
-            return <Navigate to="/"/>
+        if (this.state.modificar) {
+            return <Navigate to="/" />
         }
 
         return (
             <div className='container'>
-                <h1 className='my-5'>UpdateDepartamento <span style={{color: "red"}}>{this.props.id}</span></h1>
-                <form onSubmit={this.modificarDepartamento}>
-                    <label className='form-label'>Id: </label>
-                    <input type="text" className='form-control' defaultValue={this.state.departamento.numero} onChange={this.state.departamento.numero} disabled/>
-                    <label className='form-label mt-3'>Nombre: </label>
-                    <input type="text" className='form-control' ref={this.nombre} defaultValue={this.state.departamento.nombre} onChange={this.state.departamento.nombre}/>
-                    <label className='form-label mt-3'>Localidad: </label>
-                    <input type="text" className='form-control' ref={this.localidad} defaultValue={this.state.departamento.localidad} onChange={this.state.departamento.localidad}/>
-                    <button className='btn btn-primary mt-3'>Modificar</button>
-                </form>
+                <h1 className='my-5'>UpdateDepartamento <span style={{ color: "red" }}>{this.props.id}</span></h1>
+                {
+                    this.state.status &&
+                    <form onSubmit={this.modificarDepartamento}>
+                        <label className='form-label'>Id: </label>
+                        <input type="text" className='form-control' defaultValue={this.state.departamento.numero} disabled />
+                        <label className='form-label mt-3'>Nombre: </label>
+                        <input type="text" className='form-control' ref={this.nombre} defaultValue={this.state.departamento.nombre} />
+                        <label className='form-label mt-3'>Localidad: </label>
+                        <input type="text" className='form-control' ref={this.localidad} defaultValue={this.state.departamento.localidad} />
+                        <button className='btn btn-primary mt-3'>Modificar</button>
+                    </form>
+                }
+                <button className='btn btn-secondary mt-3' value="Mi abuela" onClick={e => this.prueba(e)}>Prueba</button>
             </div>
         )
     }
